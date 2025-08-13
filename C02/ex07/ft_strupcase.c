77897e17cc26b5a5c4d9f4ce308eddf281487606
Anton Burgstahler <aburgsta@3-D-2.42heilbronn.de>
2025-08-13 15:06:14 +0200

    added c02

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aburgsta <aburgsta@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 12:43:20 by aburgsta          #+#    #+#             */
+/*   Updated: 2025/08/13 13:15:47 by aburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'a' && str[i] <= 'a'))
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
+
+int	main()
+{
+	char test[] = "AbCdD";
+	printf("got: %s\n", ft_strupcase(test));
+}
