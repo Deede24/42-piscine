fed90d0fbff323057800a23c2d3c5d9635707273
Deede24 <128299683+Deede24@users.noreply.github.com>
2025-08-14 17:30:48 +0000

    fixed c00ex07 and done c02ex11

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -11,40 +11,37 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#define BASE 10
 
-void		ft_putnbr(int nb);
-void		ft_putchar(char c);
-
-/*
-Moulinette did KO this algorithm once. I am unsure why.
-It might be because there was an unused function ft_pow().
-*/
+void	ft_putnbr(int nb);
+void	ft_putchar(char c);
+void	ft_putdigit(int digit);
 
 void	ft_putnbr(int nb)
 {
-	long int	index;
-	long int	working_nb;
+	long	index;
+	long	working_nb;
 
 	working_nb = nb;
-	if (working_nb == 0)
-	{
-		ft_putchar('0');
-		return ;
-	}
 	if (working_nb < 0)
 	{
 		working_nb *= -1;
 		ft_putchar('-');
 	}
+	else if (working_nb == 0)
+	{
+		ft_putchar('0');
+		return ;
+	}
 	index = 1;
 	while (working_nb > index)
-	{
-		index *= 10;
-	}
+		index *= BASE;
+	if ((working_nb % index == 0))
+		ft_putchar('1');
 	while (index > 1)
 	{
-		ft_putchar('0' + ((working_nb % index) / (index / 10)));
-		index /= 10;
+		ft_putdigit((working_nb % index) / (index / BASE));
+		index /= BASE;
 	}
 }
 
@@ -53,16 +50,24 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	ft_putdigit(int digit)
+{
+	if (digit < 10)
+		ft_putchar('0' + digit);
+	else
+		ft_putchar('a' + digit - 10);
+}
+
 // int	main(void)
 // {
-	// for (int i = -2147483648; i < 2147483647; i++)
-	// {
-	// 	ft_putnbr(i);
-	// 	ft_putchar('\n');
-	// }
-	// ft_putnbr(0);
-	// ft_putnbr(-42);
-	// ft_putnbr(42);
-	// ft_putnbr(-2147483648);
-	// ft_putnbr(2147483647);
+// 	// for (int i = -2147483648; i < 2147483647; i++)
+// 	// {
+// 	// 	ft_putnbr(i);
+// 	// 	ft_putchar('\n');
+// 	// }
+// 	ft_putnbr(0);
+// 	ft_putnbr(-42);
+// 	ft_putnbr(42);
+// 	ft_putnbr(-2147483648);
+// 	ft_putnbr(2147483647);
 // }
--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -11,15 +11,63 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#define BASE 16
 
-void	ft_putstr(char *str);
+void	ft_putchar(char c);
+void	ft_puthex(int working_nb);
 
 void	ft_putstr_non_printable(char *str)
 {
 	while (*str != '\0')
 	{
 		if ((*str >= 32 && *str <= 126))
-		write(1, str, 1);
+			write(1, str, 1);
+		else
+		{
+			write(1, "\\", 1);
+			ft_puthex(*str);
+		}
 		str++;
 	}
 }
+
+void	ft_puthex(int working_nb)
+{
+	int		index;
+	char	current_digit;
+
+	if (working_nb < 0)
+	{
+		working_nb *= -1;
+		ft_putchar('-');
+	}
+	else if (working_nb == 0)
+		ft_putchar('0');
+	index = BASE * BASE;
+	if ((working_nb % index == 0) && !(working_nb == 0))
+		ft_putchar('1');
+	while (index > 1)
+	{
+		current_digit = ((working_nb % index) / (index / BASE));
+		if (current_digit < 10)
+			ft_putchar('0' + current_digit);
+		else
+			ft_putchar('a' + current_digit - 10);
+		index /= BASE;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+// int	main(void)
+// {
+// 	// ft_putstr_non_printable("Hello\nHow are you?");
+// 	// for (int i = 0; i < 188; i++)
+// 	// {
+// 	// 	ft_puthex('\n');
+// 	// 	ft_putchar('\n');
+// 	// }
+// }
